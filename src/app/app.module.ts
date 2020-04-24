830f88886962a7d9f5434c75bd12cc067caec17a
dipweb <deepak.113016@gmail.com>
2020-04-24 09:34:22 +0530

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HeaderComponent, RecipesComponent ],
+  declarations: [ AppComponent, HeaderComponent, RecipesComponent, RecipeListComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
